bcb724fdaa7b3e5426adc9b08a1d0166edb7d075
thalissongoncalves <thalissongsilva70@gmail.com>
2024-09-25 17:26:32 -0300

    inicia aplicação sabor express

---
+++ Formacao-Aprenda-A-Programar-Em-Python-Com-Orientacao-A-Objetos/01-Crie-A-Sua-Primeira-Aplicacao/sabor-express/app.py
@@ -0,0 +1,11 @@
+print("""
+𝙎𝙖𝙗𝙤𝙧 𝙀𝙭𝙥𝙧𝙚𝙨𝙨
+""")
+
+print("1. Cadastrar Restaurante")
+print("2. Listar Restaurante")
+print("3. Ativar Restaurante")
+print("4. Sair\n")
+
+opcao_escolhida = input("Escolha uma opção: ")
+print(f"Você escolheu a opção {opcao_escolhida}")

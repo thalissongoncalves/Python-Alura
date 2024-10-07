6e528ec690fec955698bfad0278f5c5c73b4e62d
thalissongoncalves <thalissongsilva70@gmail.com>
2024-10-07 17:16:06 -0300

    Feito a aula (Aprofundando em funções)

--- 01-Formacao-Aprenda-A-Programar-Em-Python-Com-Orientacao-A-Objetos/01-Crie-A-Sua-Primeira-Aplicacao/sabor-express/app.py
+++ 01-Formacao-Aprenda-A-Programar-Em-Python-Com-Orientacao-A-Objetos/01-Crie-A-Sua-Primeira-Aplicacao/sabor-express/app.py
@@ -1,25 +1,42 @@
 import os
 
-print("""
+
+def exibir_nome_do_programa():
+    print("""
 𝙎𝙖𝙗𝙤𝙧 𝙀𝙭𝙥𝙧𝙚𝙨𝙨
 """)
 
-print("1. Cadastrar Restaurante")
-print("2. Listar Restaurante")
-print("3. Ativar Restaurante")
-print("4. Sair\n")
 
-opcao_escolhida = int(input("Escolha uma opção: "))
+def exibir_opcoes():
+    print("1. Cadastrar Restaurante")
+    print("2. Listar Restaurante")
+    print("3. Ativar Restaurante")
+    print("4. Sair\n")
+
 
 def finish_app():
     os.system("cls")
     print("Encerrando o programa\n")
 
-if opcao_escolhida == 1:
-    print("Cadastrar restaurante")
-elif opcao_escolhida == 2:
-    print("Listar restaurantes")
-elif opcao_escolhida == 3:
-    print("Ativar restaurante")
-else:
-    finish_app()
+
+def escolher_opcao():
+    opcao_escolhida = int(input("Escolha uma opção: "))
+
+    if opcao_escolhida == 1:
+        print("Cadastrar restaurante")
+    elif opcao_escolhida == 2:
+        print("Listar restaurantes")
+    elif opcao_escolhida == 3:
+        print("Ativar restaurante")
+    else:
+        finish_app()
+
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+
+if __name__ == "__main__":
+    main()

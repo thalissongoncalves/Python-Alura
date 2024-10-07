90b747174170034739601004a17340395c00703b
thalissongoncalves <thalissongsilva70@gmail.com>
2024-10-07 12:40:08 -0300

    Feito a aula (if else)

--- 01-Formacao-Aprenda-A-Programar-Em-Python-Com-Orientacao-A-Objetos/01-Crie-A-Sua-Primeira-Aplicacao/sabor-express/app.py
+++ 01-Formacao-Aprenda-A-Programar-Em-Python-Com-Orientacao-A-Objetos/01-Crie-A-Sua-Primeira-Aplicacao/sabor-express/app.py
@@ -8,4 +8,12 @@ print("3. Ativar Restaurante")
 print("4. Sair\n")
 
 opcao_escolhida = input("Escolha uma opção: ")
-print(f"Você escolheu a opção {opcao_escolhida}")
+
+if opcao_escolhida == "1":
+    print("Cadastrar restaurante")
+elif opcao_escolhida == "2":
+    print("Listar restaurantes")
+elif opcao_escolhida == "3":
+    print("Ativar restaurante")
+else:
+    print("Encerrando o programa")
